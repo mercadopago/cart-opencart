ded39b4a16fad44380608145d68a3279522d0545
Bruno Codeman <brunobemfica@gmail.com>
2016-06-24 02:23:49 -0300

    improved tests

--- v2.x/tests/base_config.py
+++ v2.x/tests/base_config.py
@@ -1,19 +1,42 @@
-import json
-from splinter import Browser
+#coding: utf-8
+import time
 
+try:
+    from splinter import Browser
+except Exception, e:
+    install_command = 'sudo pip install selenium splinter'
+    print("Error on import - %s" % e)
+    print('Tip: You probably forgot to run "%s".' % install_command)
+    exit()
 
 
 class BaseConfig(object):
-	"""docstring for BaseConfig"""
-	def __init__(self):
-		self._browser = Browser()
 
-	def and(self):
-		return self
+    """docstring for BaseConfig"""
 
-	def visit_url(self, url_to_visit):
-		self._browser.visit(url_to_visit)
-	
-	def then(self):
-		return self.and()
-		
+    def __init__(self):
+        self._browser = Browser("chrome")
+
+    def visit_url(self, url_to_visit):
+        self._browser.visit(url_to_visit)
+        return self
+
+    def get_element_in_page(self, element_name):
+        return self._browser.find_by_id(element_name).first
+
+    def then(self):
+        return self
+
+
+if __name__ == '__main__':
+    try:
+        url = "http://www.mercadopago.com.br"
+        x = BaseConfig()
+        x.visit_url(url).then()\
+            .get_element_in_page("common-register") \
+            .click()
+        x.get_element_in_page("signupFirstName").value = "Henrique"
+        x.get_element_in_page("signupLastName").value = u'Juguetón'
+
+    except Exception, e:
+        print("Error - %s" % e)
