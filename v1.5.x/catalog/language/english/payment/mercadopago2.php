e20d9afd44c1cf73d7327cfa3021e010dd7db1d4
Henrique <fale.henrique@gmail.com>
2018-12-17 18:43:45 -0200

    Repository organized

--- v1.5.x/catalog/language/english/payment/mercadopago2.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Text
-$_['text_title'] = 'MercadoPago';
-$_['currency_no_support'] = 'The currency selected is not supported by MercadoPago';
-?>
--- v1.5.x/catalog/language/spanish/payment/mercadopago2.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Text
-$_['text_title'] = 'MercadoPago';
-$_['currency_no_support'] = 'La moneda seleccionado no es aceptado por MercadoPago';
-?>
